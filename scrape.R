bed386b6bf703e92f9c9c1bbb6e2443be051dfdf
Teguh Prasetyo <teguhprasetyo08@gmail.com>
2023-06-09 10:37:08 +0700

    Update coding

--- scrape.R
+++ scrape.R
@@ -1,12 +1,12 @@
 #------------------------------------------------------
-message("Loading the libraries")
+message("Load the libraries")
 library(rvest)
 library(tidyverse)
 library(dplyr)
 library(mongolite)
 
 #------------------------------------------------------
-message("Loading the URL(s)")
+message("Load the URL(s)")
 url <- "https://www.chess.com/ratings"
 link <- read_html(url)
 
@@ -15,9 +15,20 @@ message("Get Table")
 tabel <- link %>% html_nodes("table") %>% html_table() %>% as.data.frame()
 
 #------------------------------------------------------
-message("Preprocessing Table Elements")
+message("Preprocessing Data Frame Elements")
 tabel$Name <- str_squish(tabel$Name)
 tabel_fix <- tabel[,-3]
 tabel_fix$Rank <- substr(tabel_fix$Rank,nchar(tabel_fix$Rank)-3+1,nchar(tabel_fix$Rank))
 View(tabel_fix)
 
+#------------------------------------------------------
+message("Connect to MongoDB Cloud")
+atlas <- mongo(collection = "scrapingchess",
+               db = "scrapingchess",
+               url = "mongodb+srv://teguhprasetyo08:T36uh#2023@cluster1.fevi6fb.mongodb.net/?retryWrites=true&w=majority"
+)
+
+#------------------------------------------------------
+message("Store data to database")
+atlas$insert(tabel_fix)
+atlas$disconnect()
